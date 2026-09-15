db49e71116b8c144ec96385654845a1056c97dfa
Thomas Warford <twarford@swift-login-1.hpc.nlr.gov>
2026-09-15 16:37:40 -0600

    minimal NVT input

---
+++ our_trajectories/nvt_md_input.py
@@ -0,0 +1,28 @@
+from pymatgen.core import Structure, Lattice
+from pymatgen.io.vasp.sets import MatPESStaticSet
+from pymatgen.io.vasp.inputs import Kpoints
+
+incar_overrides = {
+    "EFERMI": "Midgap",
+    "ENAUG": None,
+    "GGA_COMPAT": False,
+    "ENCUT": 520,
+    # NVT ensemble (fixed cell, velocity-scaling thermostat)
+    "IBRION": 0,
+    "ISIF": 0,
+    "ISYM": 0,
+    "SMASS": 0,
+    "NSW": 2000,
+    "POTIM": 2.0,
+    "TEBEG": 1000,
+    "TEEND": 1000,
+}
+
+si = Structure(Lattice.cubic(5.43), ["Si"] * 2, [[0, 0, 0], [0.25, 0.25, 0.25]])
+
+mset = MatPESStaticSet(
+    si,
+    user_incar_settings=incar_overrides,
+    user_kpoints_settings=Kpoints.gamma_automatic((1, 1, 1)),
+)
+mset.write_input("example_nvt")
