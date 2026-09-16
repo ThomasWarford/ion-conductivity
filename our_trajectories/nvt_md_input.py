9ee966f94a5e49a260bb2afc4b2452b815863e97
Thomas Warford <twarford@swift-login-1.hpc.nlr.gov>
2026-09-16 14:26:03 -0600

    finalized settings

--- our_trajectories/nvt_md_input.py
+++ our_trajectories/nvt_md_input.py
@@ -3,10 +3,12 @@ from pymatgen.io.vasp.sets import MatPESStaticSet
 from pymatgen.io.vasp.inputs import Kpoints
 
 incar_overrides = {
+    # deviations from default MatPESStaticSet values
     "EFERMI": "Midgap",
     "ENAUG": None,
     "GGA_COMPAT": False,
-    "ENCUT": 520,
+    "ENCUT": 520, # old MPStaticSet value, so decent
+    "ISPIN": 1, # doesn't matter for most systems, and if it does this affects accuracy rather than precision
     # NVT ensemble (fixed cell, velocity-scaling thermostat)
     "IBRION": 0,
     "ISIF": 0,
@@ -16,6 +18,8 @@ incar_overrides = {
     "POTIM": 2.0,
     "TEBEG": 1000,
     "TEEND": 1000,
+    # performance
+    "NCORE": 8, 
 }
 
 si = Structure(Lattice.cubic(5.43), ["Si"] * 2, [[0, 0, 0], [0.25, 0.25, 0.25]])
@@ -23,6 +27,6 @@ si = Structure(Lattice.cubic(5.43), ["Si"] * 2, [[0, 0, 0], [0.25, 0.25, 0.25]])
 mset = MatPESStaticSet(
     si,
     user_incar_settings=incar_overrides,
-    user_kpoints_settings=Kpoints.gamma_automatic((1, 1, 1)),
+    user_kpoints_settings=Kpoints.gamma_automatic((1, 1, 1)), # also make sure to use vasp_gam rather than vasp_std for speed
 )
 mset.write_input("example_nvt")
